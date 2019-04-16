e166a2c9d307185075d2281b54a366188d8bcc92
Kate Sanders <SandersKM@Hendrix.edu>
2019-04-16 14:37:17 -0500

    progress on read/write agent file

--- app/src/main/java/com/example/hendrixassassins/agent/AgentFileHelper.java
+++ app/src/main/java/com/example/hendrixassassins/agent/AgentFileHelper.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.sql.Date;
+import java.util.ArrayList;
 
 public class AgentFileHelper {
 
@@ -19,6 +21,7 @@ public class AgentFileHelper {
         this.context = context;
     }
 
+    // https://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
     public void writeAgentListToFile(AgentList agentList, String fileName){
         try {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
@@ -32,32 +35,45 @@ public class AgentFileHelper {
     }
 
     public String getAgentListFileString(AgentList agentList){
-        String output = "";
-        // TODO iterate through each agent and write info to file
-        return output;
+        StringBuilder builder = new StringBuilder();
+        for(Agent agent: agentList.getAllAgents()){
+            builder.append(agent.getTableRow());
+        }
+        return builder.toString();
+    }
+
+    private Agent makeAgent(String line){
+        String[] split = line.trim().split(",");
+        Agent agent = new Agent(split[2], split[1]);
+        agent.setDrawNumber(Integer.parseInt(split[0]));
+        agent.setStatus(AgentStatus.valueOf(split[4]));
+        agent.setDeathTime(Date.valueOf(split[5]));
+        //TODO turn get Target Email into an Agent?
     }
 
     public AgentList getAgentListFromFile(String fileName){
         AgentList agentList = new AgentList();
-        String file = readAgentFile(fileName);
+        ArrayList<String> file = readAgentFile(fileName);
+        for(String line: file){
+
+        }
         // TODO iterate through each line and construct Agents
         return agentList;
     }
 
-    private String readAgentFile(String fileName){
-        String ret = "";
+    // https://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
+    private ArrayList<String> readAgentFile(String fileName){
+        ArrayList<String> lines = new ArrayList<>();
         try {
             InputStream inputStream = context.openFileInput(fileName);
             if ( inputStream != null ) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 String receiveString = "";
-                StringBuilder stringBuilder = new StringBuilder();
                 while ( (receiveString = bufferedReader.readLine()) != null ) {
-                    stringBuilder.append(receiveString).append("\n");
+                    lines.add(receiveString);
                 }
                 inputStream.close();
-                ret = stringBuilder.toString();
             }
         }
         catch (FileNotFoundException e) {
@@ -65,7 +81,7 @@ public class AgentFileHelper {
         } catch (IOException e) {
             Log.e("Exception", "Can not read file: " + e.toString());
         }
-        return ret;
+        return lines;
     }
 
 }
