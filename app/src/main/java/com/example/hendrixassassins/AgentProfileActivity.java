b63a4ef568ec95bf470b0f97cff43f3c4cca22a2
David Pojunas <davidapojunas@gmail.com>
2019-04-18 16:01:29 -0500

    built agent profile, and sorting buttons

--- app/src/main/java/com/example/hendrixassassins/AgentProfileActivity.java
+++ app/src/main/java/com/example/hendrixassassins/AgentProfileActivity.java
@@ -2,6 +2,8 @@ package com.example.hendrixassassins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.hendrixassassins.agent.Agent;
@@ -9,34 +11,64 @@ import com.example.hendrixassassins.agent.AgentFileHelper;
 import com.example.hendrixassassins.agent.AgentList;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AgentProfileActivity extends AppCompatActivity {
-    private String userEmail, userStatus, userScores;
     private AgentList agentList = new AgentList();
     private AgentFileHelper agentFileHelper = new AgentFileHelper();
     private ArrayList<Agent> allAgents = new ArrayList<>();
-
     private Agent thisAgent;
+    private TextView emailView, statusView, pointsView, nameView;
+    private ListView agentHistory;
 
-    private TextView nameView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agent_profile);
-        userEmail = getIntent().getStringExtra("clickedUserEmail");
-        nameView = findViewById(R.id.agentTest);
-        roadAgents();
-        setUiComponents();
+        loadAgentProfile();
+        setUpTextViews();
+        setTextViews();
+        buildAgentHistory();
+    }
+
+    private void buildAgentHistory(){
+        agentHistory = findViewById(R.id.agentHistoryList);
+        CustomListViewAdapter adapter = new CustomListViewAdapter<>(this,
+                R.layout.test_list_view, allAgents);
+
+        agentHistory.setAdapter(adapter);
     }
 
 
-    private void roadAgents(){
+    private void loadAgentProfile(){
+        String userEmail = getIntent().getStringExtra("clickedUserEmail");
+        roadAgents(userEmail);
+    }
+
+    private void setUpTextViews(){
+        emailView = findViewById(R.id.agentEmail);
+        statusView = findViewById(R.id.agentStatus);
+        pointsView = findViewById(R.id.agentPoints);
+        nameView =  findViewById(R.id.agentName);
+    }
+
+
+    private void roadAgents(String userEmail){
         agentList = agentFileHelper.getAgentListFromFile("testFile.csv", this);
         allAgents = agentList.getAllAgents();
         thisAgent = agentList.getAgentWithEmailAddress(userEmail);
     }
 
-    private void setUiComponents(){
-        nameView.setText(userEmail);
+    private void setTextViews(){
+        emailView.setText(thisAgent.getEmail());
+        statusView.setText(thisAgent.getStatus().toString());
+        pointsView.setText(String.valueOf(thisAgent.getPointsTotal()));
+        nameView.setText(thisAgent.getName());
+
+
+
+
     }
 }
--- app/src/main/java/com/example/hendrixassassins/ListViewTestActivity.java
+++ app/src/main/java/com/example/hendrixassassins/ListViewTestActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.hendrixassassins.agent.Agent;
@@ -17,6 +18,7 @@ public class ListViewTestActivity extends AppCompatActivity {
     private AgentList agentList = new AgentList();
     private AgentFileHelper agentFileHelper = new AgentFileHelper();
     private ArrayList<Agent> allAgents;
+    private Button sort_a_z, sort_z_a;
 
 
     @Override
@@ -26,21 +28,37 @@ public class ListViewTestActivity extends AppCompatActivity {
         agentList = agentFileHelper.getAgentListFromFile("testFile.csv", this);
         allAgents = agentList.getAllAgents();
         createlistViewAdapter();
+        setUpSortTestButtons();
+    }
+
+    private void setUpSortTestButtons() {
+        sort_a_z = findViewById(R.id.sort_a_z);
+        sort_a_z.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agentList.sortNamesAlphabetically();
+                rebuildListView();
+            }
+        });
+        sort_z_a = findViewById(R.id.sort_z_a);
+        sort_z_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agentList.reverse();
+                rebuildListView();
+
+            }
+        });
+    }
+
+    private void rebuildListView(){
+        allAgents = agentList.getAllAgents();
+        createlistViewAdapter();
     }
 
 
-     private void createlistViewAdapter(){
+    private void createlistViewAdapter(){
         ListView listView = findViewById(R.id.customListView_Test);
-
-//        ArrayList<String> testList = new ArrayList<>();
-//        String[] abc = new String[]{"a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-//
-//        for (String letter : abc) {
-//             testList.add(letter + "person@hendrix.edu");
-//
-//         }
-
-
         CustomListViewAdapter adapter = new CustomListViewAdapter<>(this,
                 R.layout.test_list_view, allAgents);
 
@@ -58,6 +76,7 @@ public class ListViewTestActivity extends AppCompatActivity {
                 String email = allAgents.get(position).getEmail();
                 toAgentProfileActivity(email);
             }
+
         });
     }
 
