fcacbce9250e6ab3baf0bf5b668df90f7354adc4
David Pojunas <davidapojunas@gmail.com>
2019-04-18 11:20:29 -0500

    snackbar

--- app/src/main/java/com/example/hendrixassassins/uipages/LoginActivity.java
+++ app/src/main/java/com/example/hendrixassassins/uipages/LoginActivity.java
@@ -5,8 +5,8 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
