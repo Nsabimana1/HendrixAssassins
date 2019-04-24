5510926d91add0d9fcb5a496606c6dd016709f91
Kate Sanders <SandersKM@Hendrix.edu>
2019-04-24 11:32:40 -0500

    Added purgeTime to Game

--- app/src/main/java/com/example/hendrixassassins/game/Game.java
+++ app/src/main/java/com/example/hendrixassassins/game/Game.java
@@ -9,19 +9,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class Game {
     private String email;
     private String password;
     private String fileName;
     private GameStatus status;
+    private GregorianCalendar purgeTime;
+
+    private final static String dateSplitChar = "-", spaceSplitChar = " ", timeSplitChar = ":";
+
 
     public Game(String email, String Password){
         this.email = email;
         this.password = password;
         this.fileName = this.email.split("@")[0];
         this.status = GameStatus.SIGNUP;
+    }
+
+    public void setPurgeTime(GregorianCalendar date){
+        purgeTime = date;
+    }
+
+    public GregorianCalendar getPurgeTime(){
+        return purgeTime;
     }
 
     public String getEmail() {
@@ -53,9 +68,18 @@ public class Game {
     }
 
     private String getFileContents(){
-        return getEmail() + getPassword() + getStatus().toString();
+        return getEmail() + getPassword() + getStatus().toString() + getPurgeTimeString();
     }
 
+    private String getPurgeTimeString(){
+        if(purgeTime==null){
+            return "NA";
+        }
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        return dateFormat.format(purgeTime.getTime());
+    }
+
+    // https://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
     public void writeGameToFile(Context context){
         try {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
@@ -68,12 +92,24 @@ public class Game {
         }
     }
 
+    private GregorianCalendar dateFromString(String string){
+        if(string.equals("NA") || string.trim().length() == 0) return null;
+        String[] split = string.split(spaceSplitChar);
+        String[] date = split[0].split(dateSplitChar);
+        String[] time = split[1].split(timeSplitChar);
+        return new GregorianCalendar(Integer.parseInt(date[0]), Integer.parseInt(date[1]) - 1,
+                Integer.parseInt(date[2]),  Integer.parseInt(time[0]), Integer.parseInt(time[1]),
+                Integer.parseInt(time[2]));
+    }
+
     private void updateGameFromFile(String receiveString){
         String[] split = receiveString.trim().split(",");
         resetPassword(split[1]);
         setStatus(GameStatus.valueOf(split[2]));
+        setPurgeTime(dateFromString(split[3]));
     }
 
+    // https://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
     public void readGameFromFile(Context context){
         try {
             InputStream inputStream = context.openFileInput(fileName);
