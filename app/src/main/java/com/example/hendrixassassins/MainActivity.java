87d03bf3df4724345545998045e4e11a31710795
Danny-Turner <46805389+Danny-Turner@users.noreply.github.com>
2019-04-26 23:13:56 -0500

    Added toHome button to Main Activiity

--- app/src/main/java/com/example/hendrixassassins/MainActivity.java
+++ app/src/main/java/com/example/hendrixassassins/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hendrixassassins;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +9,22 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.hendrixassassins.UItestcompnents.ListViewTestActivity;
+import com.example.hendrixassassins.agent.AgentFileHelper;
+import com.example.hendrixassassins.agent.AgentList;
 import com.example.hendrixassassins.email.Email;
 import com.example.hendrixassassins.email.EmailServer;
+import com.example.hendrixassassins.email.GmailLogin;
 import com.example.hendrixassassins.email.GmailTestActivity;
+import com.example.hendrixassassins.game.Game;
+import com.example.hendrixassassins.uipages.LoginActivity;
 
 import java.io.IOException;
 
 import javax.mail.MessagingException;
 
 public class MainActivity extends AppCompatActivity {
-    Button toGmailTest, toListViewTest, toCreateGame;
+    Button toGmailTest, toListViewTest, toCreateGame, toHome;
+    private Context context;
 
 
 
@@ -25,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         findIDs();
+        context = this.getApplicationContext();
         toGmailTestButton();
         toListViewTestButton();
+        toHomeButton();
         new FileTestActivity(getBaseContext());
         grabInbox();
     }
@@ -52,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toGmailTest = findViewById(R.id.toGmailTest);
         toListViewTest = findViewById(R.id.to_listview_test);
+        toHome = findViewById(R.id.toHome);
     }
 
     private void toGmailTestButton() {
@@ -84,5 +94,42 @@ public class MainActivity extends AppCompatActivity {
             }    }).start();
     }
 
+
+    private void toHomeButton() {
+        toHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GmailLogin("HendrixAssassinsApp","AssassinsTest1");
+                Game game = setupGame(GmailLogin.email, GmailLogin.password);
+                // go to user listview
+                gotoHomeIntent(game.getEmail());
+            }
+        });
+    }
+
+
+    private Game setupGame(String email, String password) {
+        Game game = new Game(email);
+        game.resetPassword(password);
+        game.readGameFromFile(context);
+        game.writeGameToFile(context);
+        Game game2 = new Game(email);
+        game2.readGameFromFile(context);
+        AgentFileHelper helper = new AgentFileHelper();
+        helper.writeToFile(game.getAgentFileName(), new AgentList(), context);
+        return game;
+        // TODO set next screen based on game status
+    }
+
+    private void gotoHomeIntent(String email) {
+        /*
+        Intent userListView = new Intent(LoginActivity.this, HomeActivity.class);
+        //this is where we need the game logic to tell the userListView which activity it needs to go to
+         */
+        Intent userListView = new Intent(MainActivity.this, SetUpGameActivity.class);
+        userListView.putExtra("email", email);
+        startActivity(userListView);
+        finish();
+    }
 
 }
