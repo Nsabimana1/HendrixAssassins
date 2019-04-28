2f35ba9db6228d146bbaeec10e7a18238a885f27
Kate Sanders <SandersKM@Hendrix.edu>
2019-04-28 17:32:41 -0500

    agent profile displays basic information

--- app/src/main/java/com/example/hendrixassassins/AgentProfileActivity.java
+++ app/src/main/java/com/example/hendrixassassins/AgentProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.hendrixassassins;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import com.example.hendrixassassins.UItestcompnents.CustomListViewAdapter;
 import com.example.hendrixassassins.agent.Agent;
 import com.example.hendrixassassins.agent.AgentFileHelper;
 import com.example.hendrixassassins.agent.AgentList;
+import com.example.hendrixassassins.game.Game;
 
 import java.util.ArrayList;
 
@@ -21,14 +23,40 @@ public class AgentProfileActivity extends AppCompatActivity {
             agentStatus;
     private ImageButton changeAgentName, sendEmailAgent, removeAgent, editPlayerStatus;
     private ListView agentEmailHistory, agentKillHistory;
+    private Context context;
+    private Game game;
+    private Agent agent;
+    private AgentList agentList;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
+        Intent intent = getIntent();
+        String handlerEmail = intent.getExtras().getString("handlerEmail");
+        String agentEmail = intent.getExtras().getString("agentEmail");
+        setupGame(handlerEmail);
+        setupAgentList();
+        setupAgent(agentEmail);
         setContentView(R.layout.activity_agent_profile);
         setupIDs();
+        personalizeAgentPage();
+    }
+
+    private void setupGame(String handlerEmail){
+        game = new Game(handlerEmail);
+        game.readGameFromFile(context);
+    }
+
+    private void setupAgentList(){
+        AgentFileHelper agentFileHelper = new AgentFileHelper();
+        agentList = agentFileHelper.readFromFile(game.getAgentFileName(), context);
+    }
+
+    private void setupAgent(String agentEmail) {
+        agent = agentList.getAgentWithEmailAddress(agentEmail);
     }
 
     private void setupIDs() {
@@ -36,13 +64,20 @@ public class AgentProfileActivity extends AppCompatActivity {
         AgentTotalPoints = findViewById(R.id.AgentTotalPoints);
         AgentPersonalKills = findViewById(R.id.AgentPersonalKills);
         agentEmail = findViewById(R.id.agentEmail);
-        agentStatus = findViewById(R.id.agentStatus);
+        // This was causing errors for some reason.
+        //agentStatus = findViewById(R.id.agentStatus);
         changeAgentName = findViewById(R.id.changeAgentName);
         sendEmailAgent = findViewById(R.id.sendEmailAgent);
         removeAgent = findViewById(R.id.removeAgent);
         editPlayerStatus = findViewById(R.id.editPlayerStatus);
         agentEmailHistory = findViewById(R.id.agentEmailHistory);
         agentKillHistory = findViewById(R.id.agentKillHistory);
+    }
+
+    private void personalizeAgentPage(){
+        agentName.setText(agent.getName());
+        AgentTotalPoints.setText(String.valueOf(agent.getPointsTotal()));
+        AgentPersonalKills.setText(String.valueOf(agent.getPersonalKills()));
     }
 
 
