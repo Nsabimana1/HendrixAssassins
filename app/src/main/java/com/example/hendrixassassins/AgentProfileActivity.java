3b2241e8d10a7940c510aa8d13d951caa3ccd3b6
Kate Sanders <SandersKM@Hendrix.edu>
2019-04-29 00:30:41 -0500

    Freezing agent works!

--- app/src/main/java/com/example/hendrixassassins/AgentProfileActivity.java
+++ app/src/main/java/com/example/hendrixassassins/AgentProfileActivity.java
@@ -32,7 +32,7 @@ import javax.mail.AuthenticationFailedException;
 public class AgentProfileActivity extends AppCompatActivity implements
         PopupChangeAgentName.DialogListener, PopupChangeAgentStatus.DialogListener {
     private TextView agentName, AgentTotalPoints, AgentPersonalKills, agentEmail,
-            agentStatusCurrent;
+            agentStatusCurrent, currentTarget;
     private ImageButton changeAgentName, sendEmailAgent, removeAgent, editPlayerStatus;
     private ListView agentEmailHistory, agentKillHistory;
     private Context context;
@@ -83,6 +83,7 @@ public class AgentProfileActivity extends AppCompatActivity implements
         editPlayerStatus = findViewById(R.id.editPlayerStatus);
         agentEmailHistory = findViewById(R.id.agentEmailHistory);
         agentKillHistory = findViewById(R.id.agentKillHistory);
+        currentTarget = findViewById(R.id.currentAgentTarget);
     }
 
     private void personalizeAgentPage(){
@@ -90,6 +91,7 @@ public class AgentProfileActivity extends AppCompatActivity implements
         AgentTotalPoints.setText(String.valueOf(agent.getPointsTotal()));
         AgentPersonalKills.setText(String.valueOf(agent.getPersonalKills()));
         agentStatusCurrent.setText(agent.getStatus().toString());
+        currentTarget.setText(agent.getCurrentTarget().getName());
     }
 
 
@@ -156,20 +158,76 @@ public class AgentProfileActivity extends AppCompatActivity implements
     public void changeStatus(String updatedName) {
         AgentStatus newStatus = AgentStatus.valueOf(updatedName);
         AgentStatus oldStatus = agent.getStatus();
-        if(newStatus == AgentStatus.FROZEN) freezeAgent();
-        else  agent.setStatus(newStatus);
-        agentStatusCurrent.setText(updatedName);
+        if(newStatus == AgentStatus.FROZEN) freezeAgent(agent);
+        else {
+            agent.setStatus(newStatus);
+            agentStatusCurrent.setText(updatedName);
+        }
         updateAgentListFile();
     }
 
-    private void freezeAgent() {
-        GameMethods methods = new GameMethods(agentList);
-        Agent agentWithNewTarget = agentList.getAgentAssignedToKill(agent);
+    /*private void freezeAgent() {
+        GameMethods methods = new GameMethods(agentList);;
+        Agent agentWithNewTarget = agentList.getAgentAssignedToKill(agent)
+        Log.e("PPP", agentWithNewTarget.getName());
         methods.freezeAgent(agent);
-        sendTargetEmails(agentWithNewTarget);
+        agentList = methods.getAgentList();
+        currentTarget.setText(agent.getCurrentTarget().getName());
+        Log.e("PPP", agent.getName());
+        sendFrozenEmail(agent);
+        sendTargetEmail(agentWithNewTarget);
+    }*/
+
+    private void reassignAgentWithTarget(Agent target){
+        Agent newKiller = agentList.getAgentAssignedToKill(target);
+        newKiller.setCurrentTarget(target.getCurrentTarget());
+        sendTargetEmail(newKiller);
+        target.setCurrentTarget(null);
     }
 
-    private void sendTargetEmails(Agent agent){
+    public void freezeAgent(Agent agentToFreeze){
+        reassignAgentWithTarget(agentToFreeze);
+        agentToFreeze.setStatus(AgentStatus.FROZEN);
+        sendFrozenEmail(agentToFreeze);
+        currentTarget.setText("  ");
+        currentTarget.setClickable(false);
+    }
+
+
+    private void sendFrozenEmail(Agent agent){
+        final Email message = getFrozenEmail(agent);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //GMailSender sender = new GMailSender("HendrixAssassinsApp", "AssassinsTest1");
+                    // TODO uncomment this to send emails again:
+                    GMailSender sender = new GMailSender();
+                    sender.sendMail(message);
+                } catch (AuthenticationFailedException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private Email getFrozenEmail(Agent frozenAgent) {
+        return new Email(frozenAgent.getEmail(), "Frozen Notification",
+                writeFrozenEmail(frozenAgent));
+    }
+
+    private String writeFrozenEmail(Agent agent){
+        Log.e("OOO", agent.getEmail());
+        Log.e("OOO", agent.getCurrentTargetEmail());
+        String salutation = "Dear Agent " + agent.getName() + ",\n\n";
+        String body = "You have been frozen. You have no target and no one is targeting you. \n\n";
+        String signoff = "Happy hunting,\nThe Handler";
+        return salutation + body + signoff;
+    }
+
+    private void sendTargetEmail(Agent agent){
         final Email message = getTargetEmail(agent);
         new Thread(new Runnable() {
             @Override
@@ -188,9 +246,9 @@ public class AgentProfileActivity extends AppCompatActivity implements
         }).start();
     }
 
-    private Email getTargetEmail(Agent agent){
-        return new Email(agentList.getAgentEmails(), "Assassins Verification",
-                writeTargetEmail(agent));
+    private Email getTargetEmail(Agent target){
+        return new Email(target.getEmail(), "Target Assignment",
+                writeTargetEmail(target));
     }
 
     private String writeTargetEmail(Agent agent){
